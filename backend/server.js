2b786949b8166f1861f60da547cc25c7c4e0860a
Habibashi <96149736+habibashi@users.noreply.github.com>
2023-01-06 16:02:33 +0200

    app api with port 5000

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const colors = require("colors");
+const { request } = require("http");
+const dotenv = require("dotenv").config();
+const connectDB = require("./config/db");
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.listen(port, () => console.log(`server started on port ${port}`));
